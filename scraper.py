edca9885542b91b81cacb003571c3b5e399804da
RishabhSaini <rsaini@redhat.com>
2022-12-02 13:02:09 -0500

    Fixing duplicate errors

--- scraper.py
+++ scraper.py
@@ -10,12 +10,12 @@ import os
 
 rows_per_page = '30'
 query_file = "query.json"
-result_file = "json.json"
+result_file = "result1Query.json"
 client = BodhiClient()
 query = client.query(rows_per_page=rows_per_page, content_type='rpm', releases='__current__, __pending__', status='stable')
-pages = query.pages
-#pages_list = list(range(1, pages + 1))
-#file_lock = threading.Lock()
+pages = 20#query.pages
+pages_list = list(range(1, pages + 1))
+file_lock = threading.Lock()
 #open(query_file, "w").close()
 
 def get_a_page(page_no):
@@ -49,6 +49,24 @@ def parse_nevr(s):
             ss[0] = ss[0][len(m.group(0)):]
     return ("-".join(ss))
 
+class Frequency(dict):
+    def __init__(self, build_time, alias, name):
+        self.build_time = build_time
+        self.alias = alias
+        self.name = name
+
+    def __hash__(self):
+        return hash((self.name))
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __repr__(self):
+        return '<Frequency Name: {} Alias: {} BuildTime: {}>'.format(self.name, self.alias, self.build_time)
+
+    def toDict(self):
+        return {"build_time": self.build_time, "alias": self.alias, "name": self.name}
+
 def process_data():
     query = json.load(open(query_file))
     freq = {}
@@ -63,12 +81,18 @@ def process_data():
                 name_arr.append(pkg_name)
             for name in name_arr:
                 pkg_name = parse_nevr(name)
-                pkg_name = pkg_name + "." + rows['release']['stable_tag']
+                pkg_name = pkg_name + "." + rows['release']['name']
                 if pkg_name not in freq.keys():
-                    freq[pkg_name] = [build_time]
+                    freq[pkg_name] = {Frequency(build_time, rows['alias'], name)}
                 else:
-                    freq[pkg_name].append(build_time)
+                    freq[pkg_name].add(Frequency(build_time, rows['alias'], name))
 
+    
+    for key in freq:
+        freq[key] = list(freq[key])
+        for i, item in list(enumerate(freq[key])):
+            freq[key][i] = item.toDict()
+    
     json_munch = json.dumps(freq)
 
     sys.stdout=open(result_file, "w")
@@ -76,7 +100,7 @@ def process_data():
     sys.stdout.close()
     return
 
-def main_thing():
+def init():
     with open(query_file, 'a') as f:
         f.write('[')
 
