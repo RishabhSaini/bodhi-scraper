9b44e0cacc6cd5b8fdd80bc3f437851ebfbcac36
RishabhSaini <rsaini@redhat.com>
2023-03-28 13:35:36 -0400

    Bodhiscraper combines all frequencyUpdateInfos<release> of bodhi and processes to result a file

--- container/Bodhiscraper.py
+++ container/Bodhiscraper.py
@@ -1,12 +1,19 @@
 import json
-import sys
 from bodhi.client.bindings import BodhiClient
 import re
-from concurrent.futures import ProcessPoolExecutor
-from multiprocessing import Manager
-import time
-import os
+import requests
+import xml.etree.ElementTree as ET
+import lzma
 
+# Get frequencyupdateinfo.json from all current releases of fedora and rhel and combine them to make a single json respectively.
+
+#Architecture Decision
+#Option 1 
+#Push file to ostree-config (Example: fcos/rhcos/scos config git repos)
+
+#Option 2
+#Push file directly to yum/dnf repos so it works better with Image Builder when creating bootable OS images
+    
 class Frequency(dict):
     def __init__(self, build_time, alias, name):
         self.build_time = build_time
@@ -25,6 +32,7 @@ class Frequency(dict):
     def toDict(self):
         return {"build_time": self.build_time, "alias": self.alias, "name": self.name}
 
+#Use library
 def parse_nevr(s):
     EPOCH_RE = re.compile(r"^(\d+):")
     ss = s.split('-')
@@ -45,39 +53,22 @@ def parse_nevr(s):
             ss[0] = ss[0][len(m.group(0)):]
     return ("-".join(ss))
 
-def get_a_page(client, query_file, page_no, rows_per_page, lock):
-    qe = json.dumps(client.query(rows_per_page=rows_per_page, content_type='rpm', releases='__current__, __pending__', status='stable', page=page_no)['updates'])
-    with lock:    
-        with open(query_file, 'a') as file:
-            file.write(qe)
-            file.write(',') 
-
-def setup_threads(client, query_file, pages_list, rows_per_page):
-    with Manager() as manager:
-        lock = manager.Lock()
-        with ProcessPoolExecutor() as executor:
-            futures = [executor.submit(get_a_page, client, query_file, page_no, rows_per_page, lock) for page_no in pages_list]
-            for future in futures:
-                result = future.result()
-
-def process_data(query_file, result_file):
-    query = json.load(open(query_file))
+def process_data(query, result_file):
     freq = {}
-    for page_no in range(len(query)):
-        for rows in query[page_no]:
-            if 'F' not in rows['release']['name']:
-                continue
-            pkg_name = rows['title']
-            build_time = rows['date_stable']
-            release = "fc" + rows['release']['version']
-            for build in rows['builds']:
-                nvr = build['nvr']
-                pkg_name = parse_nevr(nvr)
-                name = pkg_name + "." + release
-                if name not in freq.keys():
-                    freq[name] = {Frequency(build_time, rows['alias'], nvr)}
-                else:
-                    freq[name].add(Frequency(build_time, rows['alias'], nvr))
+    for update in query:
+        if 'F' not in update['release']['name']:
+            continue
+        pkg_name = update['title']
+        build_time = update['date_stable']
+        release = "fc" + update['release']['version']
+        for build in update['builds']:
+            nvr = build['nvr']
+            pkg_name = parse_nevr(nvr)
+            name = pkg_name + "." + release
+            if name not in freq.keys():
+                freq[name] = {Frequency(build_time, update['alias'], nvr)}
+            else:
+                freq[name].add(Frequency(build_time, update['alias'], nvr))
 
     
     for key in freq:
@@ -87,38 +78,48 @@ def process_data(query_file, result_file):
     
     json_munch = json.dumps(freq)
 
-    sys.stdout=open(result_file, "w")
-    print(json_munch)
-    sys.stdout.close()
-    return
-
-#Run a container by mounting volume
+    with open(result_file, "w") as f:
+        f.write(json_munch)
 
 def init():
-    rows_per_page = '10'
-    query_file = "./query.json"
-    result_file = "./resultOfQuery.json"
     client = BodhiClient()
-    query = client.query(rows_per_page=rows_per_page, content_type='rpm', releases='__current__, __pending__', status='stable')
+    rows_per_page = 30
+    query = client.get_releases(rows_per_page=rows_per_page, state='current')
     pages = query.pages
-    pages_list = list(range(1, pages + 1))
-    open(query_file, "w").close()
+    releases = []
+    
+    #Get all release names
+    for page_no in list(range(1, pages+1)):
+        releases.append(client.get_releases(page=page_no, rows_per_page=rows_per_page, state='current')['releases'])
+    
+    #Flatten the pages and Filter the releases
+    releases = [release for page in releases for release in page if release.id_prefix == "FEDORA"]
 
-    with open(query_file, 'a') as f:
-        f.write('[')
+    #Download the frequenctupdateinfo.json for each release and combine
+    update_infos = []
+    for release in releases:
+        repomd = requests.get(f"https://fedora.mirror.garr.it/fedora/linux/updates/{release.version}/Everything/source/tree/repodata/repomd.xml")
+        repomd_root = ET.fromstring(repomd.text)
+        ns = re.match(r'{.*}', repomd_root.tag).group(0)
+        for data in repomd_root.findall(f"{ns}data"):
+            if data.attrib['type'] == 'frequencyupdateinfo':
+                location = data.find(f"{ns}location").attrib['href']
+                zipped_update_info = requests.get(f"https://fedora.mirror.garr.it/fedora/linux/updates/{release.version}/Everything/source/tree/{location}")
+                filename = f"frequency_update_info_{release.version}.xml.xz"
+                open(filename, 'wb').write(zipped_update_info.content)
+                update_info = lzma.open(filename)
+                update_info_json = json.load(update_info)
+                for update in update_info_json:
+                    update_infos.append(update)
+    
+    #process and save file
+    process_data(update_infos, "processed_frequency.json")
 
-    start = time.perf_counter()
-    setup_threads(client, query_file, pages_list, rows_per_page)
-    finish = time.perf_counter()
-    print("Time taken: ", finish - start)
-
-    with open(query_file, 'rb+') as f:
-        f.seek(-1, os.SEEK_END)
-        f.truncate()
-
-    with open(query_file, 'a') as f:
-        f.write(']')
-
-    process_data(query_file, result_file)
+    #Option 1 
+    #Push file to ostree-config (Example: fcos/rhcos/scos config git repos)
+    
+    #Option 2
+    #Push file directly to yum/dnf repos so it works better with Image Builder when creating bootable OS images
+    
 
 init()
